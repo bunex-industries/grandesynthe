81627ba8277d2107abc24093209c4905e6c237ba
kaemu <kaemu@kaemu.com>
2019-11-22 17:42:08 +0100

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BunexHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BunexHeader from './components/BunexHeader.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BunexHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/BunexHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <p>Grande Synthé</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
